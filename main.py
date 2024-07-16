1c49d5e47ca45844a077d676d2f7739117ef2edc
Sean Meng <seanmeng2018@gmail.com>
2024-07-15 20:13:40 -0400

    ready set

---
+++ main.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+import numpy as np
+
+cap = cv.VideoCapture(0)
+
+while True:
+    _, frame = cap.read()
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    cv.imshow("window_name",hsv)
+    cv.waitKey(1)
