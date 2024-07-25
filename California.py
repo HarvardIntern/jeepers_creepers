85224694573bb23d53aa0c768c970d9d936a38db
Sean Meng <seanmeng2018@gmail.com>
2024-07-25 12:47:50 -0400

    California!!!! Homeland of the korean

--- California.py
+++ California.py
@@ -2,28 +2,37 @@ import cv2 as cv
 import numpy as np
 
 cap = cv.VideoCapture(0)
-_,img = cap.read()
+
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 def calibrate():
-
+    _, img = cap.read()
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    objp = np.zeros((6*7,3),np.float32)
-    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+    objp = np.zeros((6*9,3),np.float32)
+    objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 
     objpoints = []
     imgpoints = []
 
-    ret, corners = cv.findChessboardCorners(gray,(7,6),None)
+    ret, corners = cv.findChessboardCorners(gray,(9,6),None)
+
     if ret:
+        objpoints.append(objp)
+
         corners2 = cv.cornerSubPix(gray, corners, (11,11),(-1,-1),criteria)
-        imgpoints.append(objp)
+        imgpoints.append(corners2)
+        cv.drawChessboardCorners(img,(9,6),corners, ret)
+        ret, mtx, dist, rvecs, tvecs, = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+        return img, ret, mtx, dist, rvecs, tvecs
 
-        cv.drawChessboardCorners(img,(7,6),corners2, ret)
-    cv.imshow("img", img)
-    cv.waitKey(50000)
+    return img, 0
+
+    cv.waitKey(100)
 
 
-calibrate()
+while True:
+    a = calibrate()[0]
+    cv.imshow("sigma", a)
+    cv.waitKey(200)
 
